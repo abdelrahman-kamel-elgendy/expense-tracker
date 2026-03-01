15eaedaed98de32fd517225b8d436080c1f91580
abdelrahman-kamell <abdelrahman.kamel@si-ware.com>
2026-03-01 22:24:40 +0200

    Implement add command for expenses and initialize expenses.json with sample data

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,12 +1,46 @@
+const fs = require("fs");
 const { program } = require("commander");
+const path = require("path");
 
+const FILE_PATH = path.join(__dirname, "expenses.json");
+
+// ========= Utility Functions ========= 
+const readExpenses = () => (fs.existsSync(FILE_PATH)) ? JSON.parse(fs.readFileSync(FILE_PATH)) : [];
+
+const generateId = (expenses) => expenses.length > 0 ? Math.max(...expenses.map(e => e.id)) + 1 : 1;
+
+const formatDate = (date = new Date()) => date.toISOString().split("T")[0];
+
+const saveExpenses = (expenses) => fs.writeFileSync(FILE_PATH, JSON.stringify(expenses, null, 2));
+
+
+
+// ========= Commands ========= 
 // ADD
 program
     .command("add")
     .requiredOption("--description <desc>", "Expense description")
     .requiredOption("--amount <amount>", "Expense amount")
     .action(options => {
-        throw new Error("add command not implemented yet.");
+        const expenses = readExpenses();
+        const amount = parseFloat(options.amount);
+
+        if (amount <= 0 || isNaN(amount)) {
+            console.log("Amount must be a positive number.");
+            return;
+        }
+
+        const newExpense = {
+            id: generateId(expenses),
+            date: formatDate(),
+            description: options.description,
+            amount: amount
+        }
+
+        expenses.push(newExpense);
+        saveExpenses(expenses);
+
+        console.log(`Expense added successfully (ID: ${newExpense.id})`);
     });
 
 // LIST
